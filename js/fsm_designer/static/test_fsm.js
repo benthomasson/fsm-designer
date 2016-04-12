8d6e9cfce9fe914ffe82d8fff966e7efd4e3e1d1
Ben Thomasson <ben.thomasson@gmail.com>
2016-04-12 17:37:00 -0400

    Adding some simple unit tests

--- js/fsm_designer/static/test_fsm.js
+++ js/fsm_designer/static/test_fsm.js
@@ -1,3 +1,13 @@
-QUnit.test( "hello test", function( assert ) {
-      assert.ok( 1 == "1", "Passed!" );
-});
+var fsm = require('./fsm.js')
+
+QUnit.test( "init controller", function( assert ) {
+    var c = new fsm.Controller()
+    assert.ok(c != null, "Passed!")
+})
+
+
+QUnit.test( "Change state", function( assert ) {
+    var c = new fsm.Controller()
+    fsm.Load.fileSelected(c)
+    assert.equal(c.state, fsm.Ready, "woot!")
+})
